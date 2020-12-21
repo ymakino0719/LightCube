e5a8101a579c511c498acb4e7441dd8160c3e397
tokagero0719 <ymakino0719@gmail.com>
2020-12-21 13:48:16 +0900

    201221-2
    setup UI (temp)

---
+++ Assets/Scripts/PausedUI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausedUI : MonoBehaviour
+{
+    GameObject pausedPanel;
+    bool paused = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pausedPanel = GameObject.Find("PausedPanel");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausedPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Pausedに対応するPボタンが押されるか、Escキーが押されたらPausedパネルを開く
+        paused = Input.GetButtonDown("Paused");
+        if (Input.GetKeyDown(KeyCode.Escape)) paused = true;
+
+        if(paused) pausedPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Stage01-1");
+    }
+    public void ReturnToStageSelect()
+    {
+        SceneManager.sceneLoaded += SceneLoaded_StageSelect;
+        SceneManager.LoadScene("Title");
+    }
+    private void SceneLoaded_StageSelect(Scene next, LoadSceneMode mode)
+    {
+        Debug.Log("Hello");
+        // シーン切り替え後のスクリプトを取得
+        var tUI = GameObject.Find("UIDirector").GetComponent<TitleUI>();
+
+        // 遷移した後の処理
+        tUI.StageSelectBool = true;
+
+        // イベントから削除
+        SceneManager.sceneLoaded -= SceneLoaded_StageSelect;
+    }
+    public void HowToMove()
+    {
+        
+    }
+    public void BackToGame()
+    {
+        pausedPanel.SetActive(false);
+        paused = false;
+    }
+}
---
+++ Assets/Scripts/TitleUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleUI : MonoBehaviour
+{
+    GameObject stageSelectPanel;
+    GameObject titlePanel;
+
+    bool stageSelectBool = false;
+    void Awake()
+    {
+        titlePanel = GameObject.Find("TitlePanel");
+        stageSelectPanel = GameObject.Find("StageSelectPanel");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stageSelectPanel.SetActive(false);
+    }
+    void Update()
+    {
+        if(stageSelectBool)
+        {
+            ChangeToStageSelectPanel();
+            stageSelectBool = false;
+        }
+    }
+
+    public void ChangeToStageSelectPanel()
+    {
+        titlePanel.SetActive(false);
+        stageSelectPanel.SetActive(true);
+    }
+    public void ChangeToTitlePanel()
+    {
+        stageSelectPanel.SetActive(false);
+        titlePanel.SetActive(true);
+    }
+    public void Stage01()
+    {
+        SceneManager.LoadScene("Stage01-1");
+    }
+    public bool StageSelectBool
+    {
+        set { stageSelectBool = value; }
+        get { return stageSelectBool; }
+    }
+}
