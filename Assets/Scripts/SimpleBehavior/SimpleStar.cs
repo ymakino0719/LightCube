8a0c2e03661b379b1bcee3afdd5d8249cc11ba3b
tokagero0719 <ymakino0719@gmail.com>
2021-01-05 19:43:54 +0900

    210105-2
    change falling star (angular velocity

--- Assets/Scripts/SimpleBehavior/SimpleStar.cs
+++ Assets/Scripts/SimpleBehavior/SimpleStar.cs
@@ -6,15 +6,35 @@ public class SimpleStar : MonoBehaviour
 {
     Rigidbody rBody;
 
-    // 乱数の下限値
-    float randMin = -20.0f;
-    // 乱数の上限値
-    float randMax = 20.0f;
+    // 移動乱数の下限値
+    float moveRandMin = -20.0f;
+    // 移動乱数の上限値
+    float moveRandMax = 20.0f;
+
+    // 回転乱数の下限値
+    float rotRandMin = -2.0f;
+    // 回転乱数の上限値
+    float rotRandMax = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
-        rBody.velocity = new Vector3(Random.Range(randMin, randMax), Random.Range(randMin, randMax), Random.Range(randMin, randMax));
+
+        ChangeVelocity();
+        ChangeAngularVelocity();
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        ChangeAngularVelocity();
+    }
+    void ChangeVelocity()
+    {
+        rBody.velocity = new Vector3(Random.Range(moveRandMin, moveRandMax), Random.Range(moveRandMin, moveRandMax), Random.Range(moveRandMin, moveRandMax));
+    }
+
+    void ChangeAngularVelocity()
+    {
+        rBody.angularVelocity = new Vector3(Random.Range(rotRandMin, rotRandMax), Random.Range(rotRandMin, rotRandMax), Random.Range(rotRandMin, rotRandMax));
     }
 }
