5d7ae1bf58dcbac587f2585d361d3d4e16cce5d1
tokagero0719 <ymakino0719@gmail.com>
2021-02-26 13:44:52 +0900

    210226-6
    add aerial jump animation and others

--- Assets/Scripts/CheckIsGround.cs
+++ Assets/Scripts/CheckIsGround.cs
@@ -4,27 +4,47 @@ using UnityEngine;
 
 public class CheckIsGround : MonoBehaviour
 {
+	// Player
+	GameObject player;
+	// PlayerController
 	PlayerController pC;
+	// PlayerのRigidbody
+	Rigidbody rBody;
+
+	// 上昇中
+	bool rising = false;
+	// 上昇速度閾値
+	float risingSpeedThreshold = 0.01f;
 
 	void Awake()
 	{
-		pC = transform.parent.GetComponent<PlayerController>();
+		player = transform.parent.gameObject;
+		pC = player.GetComponent<PlayerController>();
+		rBody = player.GetComponent<Rigidbody>();
+	}
+	void Update()
+    {
+		// プレイヤーのリギッドボディのローカル速度locVelを取得
+		Vector3 locVel = player.transform.InverseTransformDirection(rBody.velocity);
+
+		// 上昇中かどうか
+		rising = (locVel.y >= risingSpeedThreshold) ? true : false;
 	}
 
 	// 着地判定
 	void OnTriggerEnter(Collider collision)
 	{
 		// 着地判定
-		if (!collision.gameObject.CompareTag("Face") && !collision.gameObject.CompareTag("Edge") && !collision.gameObject.CompareTag("Gate")) pC.IsGround = true;
+		if (!rising && !collision.gameObject.CompareTag("Face") && !collision.gameObject.CompareTag("Edge") && !collision.gameObject.CompareTag("Gate")) pC.IsGround = true;
 
 		// スイッチを踏んだ時
-		if (collision.gameObject.CompareTag("Switch")) collision.transform.parent.gameObject.GetComponent<SwitchBehavior>().CheckSwitchAndPlayerDir(collision.gameObject);
+		if (!rising && collision.gameObject.CompareTag("Switch")) collision.transform.parent.gameObject.GetComponent<SwitchBehavior>().CheckSwitchAndPlayerDir(collision.gameObject);
 	}
 
 	void OnTriggerStay(Collider collision)
 	{
 		// 着地判定
-		if (!collision.gameObject.CompareTag("Face") && !collision.gameObject.CompareTag("Edge") && !collision.gameObject.CompareTag("Gate")) pC.IsGround = true;
+		if (!rising && !collision.gameObject.CompareTag("Face") && !collision.gameObject.CompareTag("Edge") && !collision.gameObject.CompareTag("Gate")) pC.IsGround = true;
 	}
 
 	void OnTriggerExit(Collider collision)
