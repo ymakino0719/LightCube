68c3b5906fca68d1b3591786901de87f414eaa59
tokagero0719 <ymakino0719@gmail.com>
2021-03-11 15:29:51 +0900

    210311
    fix stage 1-5

--- Assets/Scripts/SimpleBehavior/SimplePlanet.cs
+++ Assets/Scripts/SimpleBehavior/SimplePlanet.cs
@@ -6,17 +6,23 @@ public class SimplePlanet : MonoBehaviour
 {
     // 子オブジェクト
     GameObject child;
-    // 回転角度
+    // 回転角度（乱数）
     float angle;
-    // 回転速度（乱数）
+    // 回転角度（固定化時）
+    public float constAngle = 0.05f;
+    // 子オブジェクトの回転速度
     float speedY;
     // 円の半径
     public float radius = 10.0f;
     // 回転方向
     public bool direction = true;
+    // 位置のランダム化
+    public bool randomPos = true;
+    // 回転角度のランダム化
+    public bool randomAngle = true;
     void Awake()
     {
-        transform.position = Random.onUnitSphere * radius;
+        if(randomPos) transform.position = Random.onUnitSphere * radius;
         child = transform.GetChild(0).gameObject;
     }
     void Start()
@@ -24,7 +30,7 @@ public class SimplePlanet : MonoBehaviour
         transform.rotation = Quaternion.LookRotation(Vector3.zero - transform.position, transform.up);
         child.transform.rotation = Random.rotation;
 
-        angle = Random.Range(0.03f, 0.07f);
+        angle = (randomAngle) ? Random.Range(0.03f, 0.07f) : constAngle;
         if (!direction) angle *= -1;
 
         speedY = Random.Range(0.5f, 1.0f);
