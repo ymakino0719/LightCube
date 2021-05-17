b3ddf7e8dd17f2c7028dd45daca373c0b5d48e56
tokagero0719 <ymakino0719@gmail.com>
2021-05-17 09:32:49 +0900

    210514
    making 2-1(temp

---
+++ Assets/Scripts/TunnelBallBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelBallBehavior : MonoBehaviour
+{
+    // PlayerのGameObject
+    GameObject player;
+    // BallのRigidbody
+    Rigidbody rBody;
+    // 仮想重力の係数
+    float gravity = 6.0f;
+
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+        rBody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        // 仮想重力をかけ続ける
+        rBody.AddForce(-player.transform.up * gravity);
+    }
+}
